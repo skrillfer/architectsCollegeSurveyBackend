8d1968a8eb50f269cf87a69c623c7af36369ac66
Luis Fernando Ramirez Santos <luisramirez@infoutilitygt.com>
2020-07-21 16:05:41 -0600

    fix3

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ db.sequelize.sync();
 require('./Sequelize/Routes/Survey.Routes')(app);
 
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/frontend/', 'build', 'index.html'));
 });
 
